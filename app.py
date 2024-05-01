3c4888b528008b8c199609910207e53412a236a2
Yeoh Joer <yeoh.joer@outlook.com>
2024-05-01 21:01:41 +0800

    Compute the feature importance using the weights of logistic regression

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
-import pandas as pd
-import streamlit as st
+from st_pages import Page
+from st_pages import show_pages
 
-from src.vis import plot_loan_default_rate_by_state, plot_top_state_by_loan
-
-df = pd.read_csv("data/SBA.csv")
-
-cols = st.columns([1, 1])
-top_state_by_loans = plot_top_state_by_loan(df)
-cols[0].altair_chart(top_state_by_loans[0])
-cols[1].altair_chart(top_state_by_loans[1])
-
-cols = st.columns([1, 1])
-cols[0].altair_chart(plot_loan_default_rate_by_state(df))
+show_pages(
+    [
+        Page("pages/visualization.py", "Visualization"),
+        Page("pages/interpretation.py", "Model Interpretation"),
+    ]
+)
