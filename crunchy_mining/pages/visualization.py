a99947d9a3a7340112f56df9682c4d364667c5a7
Yeoh Joer <yeoh.joer@outlook.com>
2024-05-13 23:20:22 +0800

    Plot the distribution of the approved loan amount by status

--- crunchy_mining/pages/visualization.py
+++ crunchy_mining/pages/visualization.py
@@ -1,12 +1,22 @@
 import pandas as pd
 import streamlit as st
 
+from crunchy_mining.vis import plot_approved_amount_by_status
 from crunchy_mining.vis import plot_loan_default_rate_by_state
 from crunchy_mining.vis import plot_top_state_by_loan
 
 st.set_page_config(layout="wide")
 
-df = pd.read_csv("data/SBA.csv")
+
+@st.cache_data
+def load_data():
+    df = pd.read_csv("data/SBA.csv")
+    df_clean = pd.read_parquet("data/output.parquet")
+
+    return df, df_clean
+
+
+df, df_clean = load_data()
 
 cols = st.columns([1, 1])
 top_state_by_loans = plot_top_state_by_loan(df)
@@ -15,3 +25,5 @@ cols[1].altair_chart(top_state_by_loans[1])
 
 cols = st.columns([1, 1])
 cols[0].altair_chart(plot_loan_default_rate_by_state(df))
+
+st.altair_chart(plot_approved_amount_by_status(df_clean), use_container_width=True)
