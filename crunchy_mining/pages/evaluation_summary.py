727f88480a47da28bf7843d1c60e5a31b91bd952
Yeoh Joer <yeoh.joer@outlook.com>
2024-05-16 14:26:47 +0800

    Add evaluation summaries of regression for approved loan amounts

--- crunchy_mining/pages/evaluation_summary.py
+++ crunchy_mining/pages/evaluation_summary.py
@@ -3,11 +3,12 @@ import streamlit as st
 
 from crunchy_mining import mlflow_util
 from crunchy_mining.util import summarize_classification
+from crunchy_mining.util import summarize_regression
 
 st.set_page_config(layout="wide")
 mlflow.set_tracking_uri("http://localhost:5001")
 
-tabs = st.tabs(["Classification", "Loan Amount", "Loan Guarantees"])
+tabs = st.tabs(["Classification", "Loan Amounts", "Loan Guarantees"])
 
 with tabs[0]:
     with st.spinner("Fetching experiment data..."):
@@ -26,6 +27,54 @@ with tabs[0]:
 
     st.altair_chart(outputs["auc_min_chart"], use_container_width=True)
     st.dataframe(outputs["auc_min"], use_container_width=True)
+
+    st.altair_chart(outputs["fit_time_chart"], use_container_width=True)
+    st.dataframe(outputs["fit_time"], use_container_width=True)
+
+    st.altair_chart(outputs["fit_time_max_chart"], use_container_width=True)
+    st.dataframe(outputs["fit_time_max"], use_container_width=True)
+
+    st.altair_chart(outputs["score_time_chart"], use_container_width=True)
+    st.dataframe(outputs["score_time"], use_container_width=True)
+
+    st.altair_chart(outputs["score_time_max_chart"], use_container_width=True)
+    st.dataframe(outputs["score_time_max"], use_container_width=True)
+
+    st.altair_chart(outputs["fit_memory_chart"], use_container_width=True)
+    st.dataframe(outputs["fit_memory"], use_container_width=True)
+
+    st.altair_chart(outputs["fit_memory_max_chart"], use_container_width=True)
+    st.dataframe(outputs["fit_memory_max"], use_container_width=True)
+
+    st.altair_chart(outputs["score_memory_chart"], use_container_width=True)
+    st.dataframe(outputs["score_memory"], use_container_width=True)
+
+    st.altair_chart(outputs["score_memory_max_chart"], use_container_width=True)
+    st.dataframe(outputs["score_memory_max"], use_container_width=True)
+
+with tabs[1]:
+    with st.spinner("Fetching experiment data..."):
+        df = mlflow_util.get_cv_metrics_by_task(task_name="bank")
+
+    outputs = summarize_regression(df)
+
+    st.altair_chart(outputs["mae_chart"], use_container_width=True)
+    st.dataframe(outputs["mae"], use_container_width=True)
+
+    st.altair_chart(outputs["mae_max_chart"], use_container_width=True)
+    st.dataframe(outputs["mae_max"], use_container_width=True)
+
+    st.altair_chart(outputs["mape_chart"], use_container_width=True)
+    st.dataframe(outputs["mape"], use_container_width=True)
+
+    st.altair_chart(outputs["mape_max_chart"], use_container_width=True)
+    st.dataframe(outputs["mape_max"], use_container_width=True)
+
+    st.altair_chart(outputs["rsq_chart"], use_container_width=True)
+    st.dataframe(outputs["rsq"], use_container_width=True)
+
+    st.altair_chart(outputs["rsq_min_chart"], use_container_width=True)
+    st.dataframe(outputs["rsq_min"], use_container_width=True)
 
     st.altair_chart(outputs["fit_time_chart"], use_container_width=True)
     st.dataframe(outputs["fit_time"], use_container_width=True)
